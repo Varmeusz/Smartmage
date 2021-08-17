f9b1d558f87f091db83c9c581b63b741ecf7a333
Adam Wacławczyk <varmeusz@magento.local>
2021-08-17 18:26:33 +0100

    Changed raki layout for css only RWD

--- app/design/frontend/Smartmage/FrontendTheme/web/js/responsive.js
+++ app/design/frontend/Smartmage/FrontendTheme/web/js/responsive.js
@@ -8,23 +8,23 @@ define(['jquery', 'matchMedia'], function($, mediaCheck)
         // Switch to Desktop Version
         entry: function () {
             console.log("Switching to Desktop...")
-            var containerCols = $('.responsive-column');
-            containerCols[1].insertAdjacentElement("beforeend", containerCols[0].children[3]);
-            containerCols[1].insertAdjacentElement("beforeend", containerCols[0].children[3]);
-            containerCols[1].insertAdjacentElement("beforeend", containerCols[0].children[3]);
-            containerCols[3].children[0].insertAdjacentElement("beforeend", containerCols[2].children[0].children[1]);
+            // var containerCols = $('.responsive-column');
+            // containerCols[1].insertAdjacentElement("beforeend", containerCols[0].children[3]);
+            // containerCols[1].insertAdjacentElement("beforeend", containerCols[0].children[3]);
+            // containerCols[1].insertAdjacentElement("beforeend", containerCols[0].children[3]);
+            // containerCols[3].children[0].insertAdjacentElement("beforeend", containerCols[2].children[0].children[1]);
             // containerCols[3].style.display = "block";
 
         },
         // Switch to Mobile Version
         exit: function () {
             console.log("Switching to Mobile...")
-            var containerCols = $('.responsive-column');
-            if(containerCols[1].childNodes.length == 0) return;
-            containerCols[0].insertAdjacentElement("beforeend", containerCols[1].children[0]);
-            containerCols[0].insertAdjacentElement("beforeend", containerCols[1].children[0]);
-            containerCols[0].insertAdjacentElement("beforeend", containerCols[1].children[0]);
-            containerCols[2].children[0].insertAdjacentElement("beforeend", containerCols[3].children[0].children[0]);
+            // var containerCols = $('.responsive-column');
+            // if(containerCols[1].childNodes.length == 0) return;
+            // containerCols[0].insertAdjacentElement("beforeend", containerCols[1].children[0]);
+            // containerCols[0].insertAdjacentElement("beforeend", containerCols[1].children[0]);
+            // containerCols[0].insertAdjacentElement("beforeend", containerCols[1].children[0]);
+            // containerCols[2].children[0].insertAdjacentElement("beforeend", containerCols[3].children[0].children[0]);
             // containerCols[3].style.display = "none";
         }
     });
