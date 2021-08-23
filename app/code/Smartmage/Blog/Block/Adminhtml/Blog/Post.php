5f2c32920a9caffca7fd41dcd361a84365f4bd4b
Adam Wacławczyk <adam.w1998@gmail.com>
2021-08-23 09:16:33 +0100

    fixed route id, added category ui component

---
+++ app/code/Smartmage/Blog/Block/Adminhtml/Blog/Post.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Smartmage\Blog\Block\Adminhtml\Blog;
+
+class Post extends \Magento\Backend\Block\Template
+{
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\Block\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    public function getMyStr(){
+        return "Post";
+    }
+}
--- app/code/Smartmage/Blog/Controller/Adminhtml/Blog/Index.php
+++ app/code/Smartmage/Blog/Controller/Adminhtml/Blog/Index.php
@@ -43,9 +43,6 @@ class Index extends Action implements HttpGetActionInterface
     public function execute()
     {
         $resultPage = $this->resultPageFactory->create();
-        $resultPage->setActiveMenu(static::MENU_ID);
-        $resultPage->getConfig()->getTitle()->prepend(__('Hello World'));
-
         return $resultPage;
     }
     
