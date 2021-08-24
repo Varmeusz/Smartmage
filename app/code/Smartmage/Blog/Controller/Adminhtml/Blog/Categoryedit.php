2c3d1201161bd71e6a3e07a34bad225103ed4ebf
Adam Wacławczyk <adam.w1998@gmail.com>
2021-08-24 13:44:23 +0100

    category form draft

---
+++ app/code/Smartmage/Blog/Controller/Adminhtml/Blog/Categoryedit.php
@@ -0,0 +1,54 @@
+<?php
+namespace Smartmage\Blog\Controller\Adminhtml\Blog;
+
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+use Smartmage\Blog\Model\Category;
+
+/**
+ * Class Post
+ */
+class Categoryedit extends Action implements HttpGetActionInterface
+{
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/exampleadminnewpage_helloworld_index.xml
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        // $resultPage->setActiveMenu("Smartmage_Blog::menu");
+        return $resultPage;
+    }
+    
+    public function _isAllowed()
+    {
+        return true;//$this->_authorization->isAllowed('Smartmage_Blog::menu');
+    }
+}
