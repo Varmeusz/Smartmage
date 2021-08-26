4bcabc2d9a60c68513980408441d03c6b2100a27
Adam Wacławczyk <adam.w1998@gmail.com>
2021-08-26 08:43:43 +0200

    Added a post massdelete action

---
+++ app/code/Smartmage/Blog/Controller/Adminhtml/Blog/Postmassdelete.php
@@ -0,0 +1,36 @@
+<?php
+namespace Smartmage\Blog\Controller\Adminhtml\Blog;
+
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Backend\App\Action\Context;
+use Magento\Ui\Component\MassAction\Filter;
+use Smartmage\Blog\Model\ResourceModel\Post\CollectionFactory;
+
+class Postmassdelete extends \Magento\Backend\App\Action
+{
+    protected $filter;
+
+    protected $collectionFactory;
+
+
+    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory)
+    {
+        parent::__construct($context, $filter);
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $collectionSize = $collection->getSize();
+        foreach ($collection as $item) {
+            $item->delete();
+        }
+
+        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', $collectionSize));
+
+        $resultRedirect = $this->resultRedirectFactory->create();
+        return $resultRedirect->setPath('*/*/post');
+    }
+}
