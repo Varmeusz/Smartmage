f9091c491abe15b8b914de72ce3d4f30ac768e20
Adam Wacławczyk <adam.w1998@gmail.com>
2021-08-26 09:07:38 +0200

    Added blog routes domain/blog, domain/blog/category, domain/blog/post

---
+++ app/code/Smartmage/Blog/Controller/Index/Index.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Smartmage\Blog\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Cms\Model\BlockFactory;
+
+/**
+ * Class Index
+ */
+class Index implements HttpGetActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    private $pageFactory;
+
+    protected $helperData;
+    
+    /**
+      * @var RequestInterface
+      */
+    private $request;
+
+    /**
+     * @param PageFactory $pageFactory
+     * @param RequestInterface $request
+     */
+    public function __construct(PageFactory $pageFactory, RequestInterface $request)
+    {
+        $this->pageFactory = $pageFactory;
+        $this->request = $request;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function execute()
+    {
+        
+        $resultPage = $this->pageFactory->create();
+
+        return $resultPage;
+    }
+}
